e570603a2434cf043c199b20b6d02a324c89ce6f
Danna Bohnhoff <dannab101@gmail.com>
2023-08-20 15:01:40 -0400

    nothing

--- views/index.jsx
+++ views/index.jsx
@@ -1,9 +1,9 @@
 const React = require('react')
 const Default = require('./layouts/default.jsx')
 
-function Index ({breads})  {
+function Index ({breads, title})  {
     return (
-        <Default>
+        <Default title={title}>
   <h2>Index Page</h2>
   {/* <p>I have {breads[0].name} bread!</p> */}
   {/* This is a JSX comment. */}
@@ -12,9 +12,10 @@ function Index ({breads})  {
   breads.map((bread, index)=> {
     return (
       <li key={index}>
-        <a href={`/breads/${index}`}> {bread.name} 
-        </a>
+        <a href={`/breads/${index}`}> 
+        
         {bread.name}
+        </a>
       </li>
     )
   })
